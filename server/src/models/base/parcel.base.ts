96e927426a726b55ff272059a1887d5f05686c21
minhd <21020307@vnu.edu.vn>
2023-12-17 16:02:12 +0700

    delete

--- server/src/models/base/parcel.base.ts
+++
@@ -1,4 +0,0 @@
-import { model } from "mongoose";
-import { IParcel, parcelSchema } from "@/models/schema/parcel.schema";
-
-export const ParcelBaseModel = model<IParcel>("Parcel", parcelSchema);
--- server/src/models/base/tracking.base.ts
+++
@@ -1,4 +0,0 @@
-import { model } from "mongoose";
-import { Itracking, trackingSchema } from "../schema/tracking.schema";
-
-export const TrackingBaseModel = model<Itracking>("Tracking", trackingSchema)
